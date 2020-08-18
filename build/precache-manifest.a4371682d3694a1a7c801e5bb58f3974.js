a43e67bdcf31ef6f9b9c8f25b93f3a692bb0c339
Tanmay Agarwal <tanmay.agarwal67@gmail.com>
2020-08-18 10:52:25 +0530

    Build Folder Included

---
+++ build/precache-manifest.a4371682d3694a1a7c801e5bb58f3974.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acbf23305fc784ceab797ecb812c5759",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de3992128e5e59d60a20",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "a26974f55aed5c186457",
+    "url": "/static/css/main.b1c749a6.chunk.css"
+  },
+  {
+    "revision": "de3992128e5e59d60a20",
+    "url": "/static/js/2.f49ab663.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f49ab663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a26974f55aed5c186457",
+    "url": "/static/js/main.dde2baca.chunk.js"
+  },
+  {
+    "revision": "b6379b58437526195da5",
+    "url": "/static/js/runtime-main.422686da.js"
+  },
+  {
+    "revision": "7190e3e3557068f65cd2120981e02ef8",
+    "url": "/static/media/covid.7190e3e3.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/static/media/spinner.235b9b3e.gif"
+  }
+]);
